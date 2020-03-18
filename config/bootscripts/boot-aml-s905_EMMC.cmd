ebb2222d86e9f1349b4fb70f0487b864783be495
balbes150 <balbes-150@yandex.ru>
2020-03-18 14:21:31 +0300

    v20200315

--- config/bootscripts/boot-aml-s905_EMMC.cmd
+++ config/bootscripts/boot-aml-s905_EMMC.cmd
@@ -1,8 +1,7 @@
-setenv env_addr 0x10400000
+setenv dtb_addr 0x1000000
+setenv env_addr 0x1040000
 setenv kernel_addr 0x11000000
 setenv initrd_addr 0x13000000
-setenv dtb_mem_addr 0x1000000
-setenv boot_start booti ${kernel_addr} ${initrd_addr} ${dtb_mem_addr}
-setenv addmac 'if printenv mac; then setenv bootargs ${bootargs} mac=${mac}; elif printenv eth_mac; then setenv bootargs ${bootargs} mac=${eth_mac}; fi'
-if fatload mmc 1 ${kernel_addr} zImage; then if fatload mmc 1 ${initrd_addr} uInitrd; then if fatload mmc 1 ${env_addr} uEnv.ini; then env import -t ${env_addr} ${filesize}; run addmac; fi; if fatload mmc 1 ${dtb_mem_addr} ${dtb_name}; then run boot_start;fi;fi;fi;
-
+setenv boot_start booti ${kernel_addr} ${initrd_addr} ${dtb_addr}
+setenv addmac 'if printenv mac; then setenv bootargs ${bootargs} mac=${mac}; elif printenv eth_mac; then setenv bootargs ${bootargs} mac=${eth_mac}; elif printenv ethaddr; then setenv bootargs ${bootargs} mac=${ethaddr}; fi'
+if fatload mmc 0 ${env_addr} uEnv.txt && env import -t ${env_addr} ${filesize}; setenv bootargs ${APPEND}; then if fatload mmc 0 ${kernel_addr} ${LINUX}; then if fatload mmc 0 ${initrd_addr} ${INITRD}; then if fatload mmc 0 ${dtb_addr} ${FDT}; then run addmac; run boot_start; fi; fi; fi; fi;
